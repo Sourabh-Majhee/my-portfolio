57868ca24069d4b931d8515d63537bc79a034624
Sourabh Majhee <sourabhmajhee605@gmail.com>
2025-09-05 15:12:18 +0530

    added backdrop-blur in Navbar section, so that the overlap looks decent

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -25,11 +25,11 @@ export const Navbar = () => {
   return (
     <nav
       className={cn(
-        "fixed w-full z-40 transition-all duration-300",
+        "fixed w-full z-40 transition-all duration-300 backdrop-blur",
         isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
       )}
     >
-      <div className="container flex items-center justify-between">
+      <div className="container flex items-center justify-between ">
         <a
           className="text-xl font-bold text-primary flex items-center"
           href="#hero"
